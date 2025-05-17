b47400b14a704fbf95e9cd833d82de4e25131bdd
xChrisDev <chrissarmiento200203@gmail.com>
2025-05-17 13:21:43 -0600

    feat: fetching role

--- frontend-vue/src/components/session-page/SignUpForm.vue
+++ frontend-vue/src/components/session-page/SignUpForm.vue
@@ -1,12 +1,42 @@
 <script setup>
 import { ref } from 'vue';
 import NeoButton from '../NeoButton.vue';
+import { registerUser } from '../../services/userService';
 
 const form = ref({
     email: '',
     username: '',
-    password: ''
+    password: '',
+    role: ''
 });
+
+const isDropdownOpen = ref(false);
+const roleOptions = ['Mentor', 'Estudiante'];
+
+function selectOption(option) {
+    form.value.role = option;
+    isDropdownOpen.value = false;
+}
+
+const fetchRegister = async () => {
+    let role_selected = ""
+
+    if (form.value.role == "Mentor") {
+        role_selected = "mentor"
+    } else if (form.value.role = "Estudiante") {
+        role_selected = "student"
+    }
+
+    const user = {
+        email: form.value.email,
+        username: form.value.username,
+        password: form.value.password,
+        role: role_selected
+    }
+
+    const res = await registerUser();
+    console.log(res)
+}
 </script>
 
 <template>
@@ -45,6 +75,30 @@ const form = ref({
             </div>
         </div>
 
-        <NeoButton text="Registrarse" bg="#96FEAD" icon="login" class="mt-4 justify-center" />
+        <div class="flex flex-col gap-1 w-full relative">
+            <label class="font-semibold text-sm lg:text-base"> Tipo de usuario </label>
+
+            <!-- Caja principal -->
+            <div class="flex items-center border-[3px] border-black rounded-xl bg-white px-4 py-2 relative cursor-pointer"
+                @click="isDropdownOpen = !isDropdownOpen">
+                <span class="material-symbols-rounded text-gray-500 me-2" style="font-size: 1.2rem;">
+                    diversity_3 </span>
+                <span class="text-sm lg:text-base select-none">
+                    {{ form.role || 'Selecciona una opción' }}
+                </span>
+                <span class="material-symbols-rounded absolute right-4 text-gray-500"> expand_more </span>
+            </div>
+
+            <!-- Opciones -->
+            <div v-show="isDropdownOpen"
+                class="absolute top-full mt-2 left-0 right-0 border-[3px] border-black rounded-xl bg-white z-50">
+                <div v-for="option in roleOptions" :key="option" @click="selectOption(option)"
+                    class="px-4 py-2 rounded-xl cursor-pointer hover:bg-gray-200 text-sm lg:text-base border-black last:border-none">
+                    {{ option }}
+                </div>
+            </div>
+        </div>
+
+        <NeoButton @click="fetchRegister" text="Registrarse" bg="#96FEAD" icon="login" class="mt-4 justify-center" />
     </form>
 </template>
